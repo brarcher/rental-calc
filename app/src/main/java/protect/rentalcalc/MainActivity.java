7704e1b8b466e19debcea02a74e1d4bcfebf659f
Branden Archer <b.m.archer4@gmail.com>
2017-03-15 22:20:18 -0400

    Add About dialog

--- app/src/main/java/protect/rentalcalc/MainActivity.java
+++ app/src/main/java/protect/rentalcalc/MainActivity.java
@@ -1,11 +1,26 @@
 package protect.rentalcalc;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Calendar;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
 {
+    private static final String TAG = "RentalCalc";
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -13,5 +28,102 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.properties_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if(id == R.id.action_about)
+        {
+            displayAboutDialog();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void displayAboutDialog()
+    {
+        final Map<String, String> USED_LIBRARIES = ImmutableMap.of
+        (
+            "Guava", "https://github.com/google/guava"
+        );
+
+        StringBuilder libs = new StringBuilder().append("<ul>");
+        for (Map.Entry<String, String> entry : USED_LIBRARIES.entrySet())
+        {
+            libs.append("<li><a href=\"").append(entry.getValue()).append("\">").append(entry.getKey()).append("</a></li>");
+        }
+        libs.append("</ul>");
+
+        String appName = getString(R.string.app_name);
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        String version = "?";
+        try
+        {
+            PackageManager manager = getPackageManager();
+            if(manager != null)
+            {
+                PackageInfo pi = manager.getPackageInfo(getPackageName(), 0);
+                version = pi.versionName;
+            }
+            else
+            {
+                Log.w(TAG, "Package name not found, PackageManager unavailable");
+            }
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            Log.w(TAG, "Package name not found", e);
+        }
+
+        WebView wv = new WebView(this);
+        String html =
+                "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />" +
+                        "<img src=\"file:///android_res/mipmap/ic_launcher.png\" alt=\"" + appName + "\"/>" +
+                        "<h1>" +
+                        String.format(getString(R.string.about_title_fmt),
+                                "<a href=\"" + getString(R.string.app_webpage_url)) + "\">" +
+                        appName +
+                        "</a>" +
+                        "</h1><p>" +
+                        appName +
+                        " " +
+                        String.format(getString(R.string.debug_version_fmt), version) +
+                        "</p><p>" +
+                        String.format(getString(R.string.app_revision_fmt),
+                                "<a href=\"" + getString(R.string.app_revision_url) + "\">" +
+                                        getString(R.string.app_revision_url) +
+                                        "</a>") +
+                        "</p><hr/><p>" +
+                        String.format(getString(R.string.app_copyright_fmt), year) +
+                        "</p><hr/><p>" +
+                        getString(R.string.app_license) +
+                        "</p><hr/><p>" +
+                        String.format(getString(R.string.app_libraries), appName, libs.toString());
+
+
+        wv.loadDataWithBaseURL("file:///android_res/drawable/", html, "text/html", "utf-8", null);
+        new AlertDialog.Builder(this)
+                .setView(wv)
+                .setCancelable(true)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
