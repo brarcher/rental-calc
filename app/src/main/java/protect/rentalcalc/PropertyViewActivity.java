6c20e51c81e0be5e07053b5d007b624bd3357e57
Branden Archer <b.m.archer4@gmail.com>
2017-03-27 23:46:57 -0400

    Remove unused imports in PropertyViewActivity

--- app/src/main/java/protect/rentalcalc/PropertyViewActivity.java
+++ app/src/main/java/protect/rentalcalc/PropertyViewActivity.java
@@ -1,11 +1,7 @@
 package protect.rentalcalc;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
