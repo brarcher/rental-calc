0ec58952602b2adb3afabec96ef5d562b2e91f9f
Branden Archer <b.m.archer4@gmail.com>
2017-05-02 23:33:11 -0400

    Add class for property calculations
    The calculations will be used in multiple places. This
    puts the computation in one place.

---
+++ app/src/main/java/protect/rentalcalc/PropertyCalculation.java
@@ -0,0 +1,61 @@
+package protect.rentalcalc;
+
+public class PropertyCalculation
+{
+    // INCOME:
+
+    // Takes into account income increase
+    double grossRent;
+
+    // percentage of gross rent
+    double vacancy;
+
+    // gross rent - vacancy
+    double operatingIncome;
+
+    // EXPENSES:
+
+    // Takes into account expense increase
+    double totalExpenses;
+
+    // operating income - total expenses
+    double netOperatingIncome;
+
+    // CASH FLOW:
+
+    // Only for the years that the loan is being paid off
+    double loanPayments;
+
+    // net operating income - loan payments
+    double cashFlow;
+
+    // EQUITY:
+
+    // Take into account appreciation
+    double propertyValue;
+
+    // Amount left to pay off
+    double loanBalance;
+
+    // property value - loan balance
+    double totalEquity;
+
+    // TAX BENEFITS:
+
+    // In the US, only for the first 27.5 years of ownership.
+    // Takes into account the (purchase price - land value + purchase costs) / 27.5
+    double depreciation;
+
+    // The amount of interest paid on the loan
+    double loanInterest;
+
+    // RETURNS:
+
+    double capitalization;
+
+    double cashOnCash;
+
+    double rentToValue;
+
+    double grossRentMultiplier;
+}
